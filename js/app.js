69c3f3c33e7d2a94da97def994408103a22b368f
Bobby Sixkiller <jansoldat68@gmail.com>
2019-04-03 15:43:24 +0200

    Started working on multilingual

--- js/app.js
+++ js/app.js
@@ -26,11 +26,12 @@ window.onscroll = () => {
 window.onresize = () => {
     ui.correctProportions();
     ui.toggleNavbarColor(_navbar, _navbarBrand);
-    initMap();
+   // initMap();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    initMap();
+    chooseLang();
+   // initMap();
     createWinesGrid('./data/supliers.json');
     ui.smoothScroll();
 });
@@ -56,4 +57,68 @@ function initMap() {
     // The location of WineArt
     map.createInfoWindow();
     map.createMap(map.createInfoWindow());
+}
+
+
+let arrLang = {
+    'en': {
+        'about' : 'About Us',
+        'services' : 'Services',
+        'offer' : 'Offer',
+        'contact': 'Contact',
+        /** ABOUT **/
+        'about--text-one':'Our philosophy is to expand among the wine lovers a something little bit different and more exlusive than is the current offer from big producers. Everything is based on personal contact and cooperation. Our motto says that ',
+        'about--text-two':'life is too short to drink poor wine!',
+        'about--text-three':'We collaborate with small family wineries on a personal basis. As a part of our activities we often organize wine tasting. Both in our place or directly at customers, where we are trying to spread ideas from individual wineris and their approach to the wine production.',
+        'about--text-fourth':'If you have any questions or suggestions, don\'t hesitate to ',
+        'about--text-fifth':'contact us', 
+        'about--text-sixth':'We are looking forward to enjoy a glass of our preeminent products together!'
+        /** SERVICES **/
+    },
+    'cs': {
+        'about' : 'O nás',
+        'services' : 'Služby',
+        'offer' : 'Nabídka',
+        'contact': 'Kontakt',
+        'about--text-one':'Naší filosofií je dostat mezi milovníky vína něco trochu jiného a exluzivnějšího, než je aktuální nabídka velkoproducentů. Reprezentujeme pouze svá vína na webu nebo eshopu, vše je na bázi osobní spolupráce. Naše motto zní, že ',
+        'about--text-two':'život je příliš krátký na to, abychom pili nekvalitní víno.',
+        'about--text-three':'Spolupracujeme s malými rodinnými vinařstvími na osobní bázi. V rámci svých aktivit pořádáme často degustace jak u nás, tak přímo u zákazníků, kde se právě snažíme co nejvíce přiblížit myšlenky jednotlivých vinařství a jejich přístup k výrobě vína.',
+        'about--text-fourth':'V případě jakýchkoliv dotazů či podmětů nás neváhejte ',
+        'about--text-fifth':'kontaktovat', 
+        'about--text-sixth':'Těšíme se až si společně vychutnáme sklenku našich prvotřídních produktů!'
+    }
+}
+
+
+
+
+// LANG
+
+function chooseLang() {
+    const lang =localStorage.getItem('language');
+    console.log(lang);
+    
+    if (lang !== null) {
+        changeLang(lang);
+    }
+
+};
+
+document.querySelectorAll('.translate').forEach((langBTN) => {
+    langBTN.addEventListener('click', (e)=>{           
+        localStorage.setItem('language', e.target.id);
+        changeLang(e.target.id);
+        
+    });
+
+});
+
+
+
+
+function changeLang(lang) {
+    document.querySelectorAll('.lang').forEach((element) => {
+        const key = element.getAttribute('lang-key');
+        element.innerText = arrLang[lang][key];
+    });
 }
